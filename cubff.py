2ab0c2d0b7a76aff82682358fa104ae8da4740b4
Robert Obryk <robryk@google.com>
2024-07-16 16:11:56 +0200

    wip: reruns

--- cubff.py
+++ cubff.py
@@ -12,17 +12,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
 from bin import cubff
 
 
-def callback(state):
-    print(state.epoch, state.brotli_size)
-    state.print_program(1)
-    return state.epoch > 1024
+INIT_SEED = 0
+THRESHOLD_ENTROPY = 3
 
 
-params = cubff.SimulationParams()
-params.num_programs = 131072
-params.seed = 0
+def StandardParams():
+  params = cubff.SimulationParams()
+  params.num_programs = 131072
+  params.seed = INIT_SEED
+  params.load_from = sys.argv[1]
+  params.mutation_prob = int(round(0.001*(1<<30)))
+  return params
 
-cubff.RunSimulation("bff_noheads", params, None, callback)
+def find_threshold_epoch(params):
+  initial_epoch = None
+  epochs = 0
+  ok = False
+  
+  def callback(state):
+    nonlocal initial_epoch
+    if not initial_epoch:
+      initial_epoch = state.epoch
+    nonlocal epochs
+    epochs = state.epoch
+    nonlocal ok
+    ok = state.higher_entropy > THRESHOLD_ENTROPY
+    return ok or state.epoch > 32*1024 + initial_epoch
+  
+  cubff.RunSimulation("bff_noheads", params, None, callback)
+
+  if ok:
+    return epochs
+  return None
+
+params = StandardParams()
+for s in range(100):
+  params.seed = INIT_SEED + s
+  print(s, find_threshold_epoch(params))
